ad8af50fb6f5915950fb245bf1c9644df3443c8e
markcech <marketa.sebestova@gmail.com>
2023-04-28 22:57:15 +0200

    druhý úkol

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,7 +1,7 @@
 describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
-
+//ÚKOL 1
         await browser.reloadSession();
 
         await browser.url('/registrace');
@@ -9,6 +9,32 @@ describe('Homework', async () => {
         await browser.saveScreenshot('registrace.png');
 
         await browser.pause(5000);
+
+//ÚKOL 2
+        await browser.url('/registrace');
+
+        const nameField = await $('#name');
+        console.log(await nameField.getHTML());
+        
+        const emailField = await $('#email');
+        console.log(await emailField.getHTML());
+        
+        const passwordField = await $('#password');
+        console.log(await passwordField.getHTML());
+        
+        const passwordConfirmField = await $('#password-confirm');
+        console.log(await passwordConfirmField.getHTML());
+        
+        const registrationButton = await $('.btn-primary');
+        console.log(await registrationButton.getHTML());
+
+
+
+
+
+
+
+
 
     });
 
