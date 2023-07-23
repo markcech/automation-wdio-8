9541d2859579b5aa11beb97272322c6579623bde
markcech <marketa.sebestova@gmail.com>
2023-07-23 23:45:10 +0200

    email generator

--- test/specs/homework/specs/fixtures.js
+++ test/specs/homework/specs/fixtures.js
@@ -1,4 +1,11 @@
+function generateEmail (username) {
+
+  const randomNumber = Math.floor(Math.random() * 100);
+
+  return username + randomNumber + "@email.cz";
+}
+
 export const name = 'John Soe';
 export const passwordRegistration = 'John123456';
 export const passwordRegistrationConfirm = 'John123456';
-export const email = 'john.soe@seznam.cz';
+export const email = generateEmail(name.split(' ').join('.'));
